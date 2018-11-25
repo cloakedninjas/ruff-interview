72698ae1e662938d55fb4b7fd9a0a79d6d0d9ae3
Daniel Jackson <daniel@cloakedninjas.co.uk>
2018-11-25 10:06:01 +0000

    Final changes

--- src/refs.d.ts
+++ src/refs.d.ts
@@ -1,1 +1,14 @@
 /// <reference path="../node_modules/phaser-ce/typescript/phaser.comments.d.ts" />
+
+//grunt-start
+/// <reference path="scripts/entity/dog.ts" />
+/// <reference path="scripts/entity/preload-bar.ts" />
+/// <reference path="scripts/entity/question-manager.ts" />
+/// <reference path="scripts/entity/text-button.ts" />
+/// <reference path="scripts/game.ts" />
+/// <reference path="scripts/state/boot.ts" />
+/// <reference path="scripts/state/end.ts" />
+/// <reference path="scripts/state/game.ts" />
+/// <reference path="scripts/state/preloader.ts" />
+/// <reference path="scripts/state/title.ts" />
+//grunt-end
